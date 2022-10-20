fca5132a10cc252b54395ece6aba3829c71563e8
kyle65463 <kyle65463@gmail.com>
2022-10-20 09:29:41 +0800

    Fix result course list paddings

--- src/components/ResultCourseList.tsx
+++ src/components/ResultCourseList.tsx
@@ -10,7 +10,7 @@ const ResultCourseList = () => {
     return resultCourseIds.map((id) => courseIdsMap[id] ?? emptyCourse);
   }, [resultCourseIds, courses]);
   return (
-    <section className="mt-8 flex flex-wrap items-center justify-center">
+    <section className="mt-8 flex flex-wrap items-center justify-center gap-1.5">
       {/* No result hint */}
       <span className="mr-1.5 font-medium">
         {resultCourseIds.length > 0 ? "Result:" : "No Result"}
@@ -19,7 +19,7 @@ const ResultCourseList = () => {
       {/* Result course titles  */}
       {resultCourses.map((course) => (
         <span
-          className="mr-1.5 mb-1.5 rounded bg-gray-200 px-1.5 py-0.5 text-sm text-gray-900"
+          className="rounded bg-gray-200 px-1.5 py-0.5 text-sm text-gray-900"
           key={course.id}
         >
           {course.title}
